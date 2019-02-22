bee6d8c19fb221447ab1a323d2d425a9d7eb8fe5
Hemant <hemant.phutela@gmail.com>
2019-02-21 22:07:14 -0500

    Added Spin functionalty

--- MAPD724-TEST1/GameScene.swift
+++ MAPD724-TEST1/GameScene.swift
@@ -14,6 +14,11 @@ var background = SKSpriteNode(imageNamed: "background")
 var spinButtonnode:SKSpriteNode!
 var resetButtonnode:SKSpriteNode!
 var quitButtonnode:SKSpriteNode!
+var spinImage1node:SKSpriteNode!
+var spinImage2node:SKSpriteNode!
+var spinImage3node:SKSpriteNode!
+var textures = [SKTexture]()
+
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
@@ -26,13 +31,50 @@ class GameScene: SKScene {
       spinButtonnode = self.childNode(withName: "spinButton") as? SKSpriteNode
       quitButtonnode = self.childNode(withName: "quitButton") as? SKSpriteNode
         resetButtonnode = self.childNode(withName: "resetButton") as? SKSpriteNode
-       
+       spinImage1node = self.childNode(withName: "spinImage1") as? SKSpriteNode
+        spinImage2node = self.childNode(withName: "spinImage2") as? SKSpriteNode
+        spinImage3node = self.childNode(withName: "spinImage3") as? SKSpriteNode
         
         
        spinButtonnode.texture = SKTexture(imageNamed: "Spin")
         resetButtonnode.texture = SKTexture(imageNamed: "Reset")
         quitButtonnode.texture = SKTexture(imageNamed: "quit")
         
+        textures.append(SKTexture(imageNamed: "Cherry"))
+        textures.append(SKTexture(imageNamed: "Apple"))
+        textures.append(SKTexture(imageNamed: "Grape"))
+        textures.append(SKTexture(imageNamed: "Kiwi"))
+        textures.append(SKTexture(imageNamed: "Lemon"))
+        textures.append(SKTexture(imageNamed: "Banana"))
+        
 }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            let nodesArray = self.nodes(at: location)
+            
+            if nodesArray.first?.name == "spinButton"{
+                
+                // Randomly select a character
+                let rand = Int(arc4random_uniform(UInt32(textures.count)))
+                let rand2 = Int(arc4random_uniform(UInt32(textures.count)))
+               let rand3 = Int(arc4random_uniform(UInt32(textures.count)))
+                let texture = textures[rand] as SKTexture
+                let texture2 = textures[rand2] as SKTexture
+                 let texture3 = textures[rand3] as SKTexture
+                spinImage1node.texture = texture
+                spinImage2node.texture = texture2
+                spinImage3node.texture = texture3
+                
+               // spinImage1node.texture = SKTexture(imageNamed: "Cherry")
+                //spinImage2node.texture = SKTexture(imageNamed: "Apple")
+                //spinImage3node.texture = SKTexture(imageNamed: "Grape")
+               
+                
+            }
+    
+    
+}
+}
 }
